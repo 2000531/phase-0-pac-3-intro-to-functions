c5d3871fe37b7390b898fd72f47917a0c30e8a40
Ronoh_Collins <collins.ronoh@student.moringaschool.com>
2025-03-23 22:47:11 +0300

    thafu labs

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
 // Follow along with the examples here
+function doNothing() {}
+function sayHello() {
+  console.log("Hello!");
+}
+
+sayHello(); 
+
+function sayHelloTOGuadalupe() {
+console.log("Hello to Guadalupe");
+}
+sayHelloTOGuadalupe();
+
+function sayHelloToLiz() {
+    console.log("Hello to Liz");    
+}
+sayHelloToLiz();
+
+function sayHelloToSamip() {
+    console.log("Hello to Samip");
+    
+}function sayHelloTo(firstName) {
+  console.log(`Hello, ${firstName}!`);
+}
+
+sayHelloTo("Guadalupe"); // "Hello, Guadalupe!"
+sayHelloTo("Jane"); // "Hello, Jane!"
+sayHelloTo("R2-D2"); // "Hello, R2-D2!"
+sayHelloTo(1); // "Hello, 1!"
+function say(greeting, firstName) {
+    console.log(`${greeting}, ${firstName}!`);
+  }
+  say("Hello", "Julio");
+  console.log (say("Hello","Lizzzz"));
+  
+
+
+  function add(x, y) {
+    return x + y;
+  }
+  add("9000","82");
+  console.log(add(9000,82));
+  
